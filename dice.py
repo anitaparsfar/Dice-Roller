5f32d5eb4ce6aeb884c94d1904ee092dbf39173a
Anita <anita.parsfar@gmail.com>
2025-09-02 14:53:22 +0330

    finished code

--- dice.py
+++ dice.py
@@ -1,1 +1,47 @@
-print("Welcome to Dice Roller!")
+import random
+# ● ┌ ─ ┐ │ └ ┘
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│ ●       │",
+        "│         │",
+        "│       ● │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│ ●       │",
+        "│    ●    │",
+        "│       ● │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│ ●     ● │",
+        "│         │",
+        "│ ●     ● │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│ ●     ● │",
+        "│    ●    │",
+        "│ ●     ● │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│ ●     ● │", 
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "└─────────┘"),    
+}
+dice = []
+total = 0
+num_dice = int(input("How many dices do you want? "))
+for die in range(num_dice):
+    dice.append(random.randint(1, 6))
+    
+arts = [dice_art[val] for val in dice]
+for line in zip(*arts):
+    print("   ".join(line))
+      
+total = sum(dice)
+print(f"Your rolled: {dice}")
+print(f"Total: {total}")
